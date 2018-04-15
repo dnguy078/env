15aab8848c9329f3c7f560daca8d4730d6e33eb5
Dennis Nguyen <dnguy078@ucr.edu>
2018-04-15 07:09:10 -0700

    comment

--- startup.sh
+++ startup.sh
@@ -4,7 +4,8 @@ echo "Starting startup script"
 xrandr --dpi 200
 xflux -z 92805
 xset r rate 250 60
-#multiple screens
+# multiple screens
+# replace - above to --right-of / left-of for position
 xrandr --output eDP1 --primary --auto --output DP1 --above eDP1 --auto
 xmodmap -e "keycode 133 = Control_L"
 xmodmap -e "keycode 37 = Overlay1_Enable"
